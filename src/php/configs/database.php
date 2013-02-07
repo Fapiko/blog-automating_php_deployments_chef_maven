73138002e242eb1c67c54658985870433a894eca
Lucas Jandrew <lucas@jandrew.com>
2013-02-07 00:49:21 -0800

    Adding some private files to show how to exclude stuff from the package

---
+++ src/php/configs/database.php
@@ -0,0 +1,7 @@
+<?php
+$database = array(
+	'hostname' => '127.0.0.1',
+	'username' => 'arnoldfacepalmer',
+	'password' => 'topsekretpassword',
+	'name' => 'awesomestdbevar'
+);
---
+++ src/php/configs/languages.php
@@ -0,0 +1,6 @@
+<?php
+$languages = array(
+	'en' => 'en_US',
+	'es' => 'es_ES',
+	'de' => 'de_DE'
+);
